83ab8d783c5855f61ce457ec3a0a6f997541c456
RyoichiNakai <elmobasketball0913@me.com>
2020-09-18 15:17:00 +0900

    [Add]

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,16 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(function(){
+    // inputのidから情報の取得
+    $('.upload-img').on('change', function (e) {
+// ここから既存の画像のurlの取得
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $(".item_image").attr('src', e.target.result);
+        }
+// ここまで
+        reader.readAsDataURL(e.target.files[0]); //取得したurlにアップロード画像のurlを挿入
+    });
+});
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'f067c120824d815f9c48675893c6d88053cb3950be7d47de663744325730358dda7feeefeace323f9dc57e5cce901bed31af0adaf0c600eb43b9aa69f849e40e'
